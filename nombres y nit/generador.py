df599748e99e0ad34a3a47845970fc72f8b981ea
Antonio <reytono@gmail.com>
2019-05-14 00:18:48 -0600

    generar nombres apellidos y nit al azar

---
+++ nombres y nit/generador.py
@@ -0,0 +1,25 @@
+from random import randint
+
+
+nombres = []
+apellidos = []
+
+f = open("nombres.txt", "r")
+for x in f:
+    separado = x.split()
+    nombres.append(separado[0])
+    apellidos.append(separado[1])
+
+
+randNIT = randint(100000, 1000000) 
+
+contador = 100
+
+while contador > 0:
+    randNIT = randint(100000, 1000000) 
+    varchar = str(randNIT)
+    nit_guion = varchar[0:len(varchar) -1 ] + "-" + varchar[len(varchar) - 1: len(varchar)]
+
+    print(nombres[randint(0, len(nombres) - 1 )], apellidos[randint(0, len(nombres) - 1 )], nit_guion)
+    contador -= 1
+    
