69cc4be9772bbe403580a12ba11fdef075291873
Antonio Reyes <35511339+T0N1R@users.noreply.github.com>
2019-06-05 21:38:30 -0600

    Update twitterScript.py

--- twitter/twitterScript.py
+++ twitter/twitterScript.py
@@ -1,9 +1,11 @@
 import twitter
+import pymongo
+import threading
 
-consumer_key = ' '
-consumer_secret = ' '
-access_token = ' '
-access_token_secret = ' '
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 
 api = twitter.Api(consumer_key=consumer_key,
@@ -11,8 +13,54 @@ api = twitter.Api(consumer_key=consumer_key,
                   access_token_key=access_token,
                   access_token_secret=access_token_secret)
 
-#print(api.VerifyCredentials())   
+def hacer_tweet():
+    lista = []
+    lista_enviar = []
 
-post_update = api.PostUpdates(status = 'Prueba con python-twitter: {}, {}, {}'.format("usuario1", "usuario2", "usuario3"))
+    connection = pymongo.MongoClient('localhost', 27017)
 
-print(post_update)
+    database = connection ['mydb_01']
+
+    collectionCliente = database['consolidado']
+
+    pipeline = [
+        {"$group" : {"_id" : "$nombre", "count" : {"$sum" : "$total"}}},
+        {"$sort" : {"count": -1}},
+        {"$limit" : 3}
+    ]
+
+    cursos = collectionCliente.aggregate(pipeline)
+
+    for x in cursos:
+        lista.append(x)
+
+    for i in lista:
+        linea = str(i)
+
+        separar = linea.split(', ')
+
+        separar2 = separar[0].split(': ')
+
+        enviar = separar2[1][1:len(separar2[1])-1]
+
+        lista_enviar.append(enviar)
+
+    primero = lista_enviar[0]
+    segundo = lista_enviar[1]
+    tercero = lista_enviar[2]
+
+
+    post_update = api.PostUpdates(status = 'Prueba de proyecto: 1. {}, \n  2. {}, \n 3. {}'.format(primero, segundo, tercero))
+
+    print(post_update)
+
+def imprimirHola():
+    print("Hello World")
+
+def printit():
+  threading.Timer(20.0, printit).start()
+  hacer_tweet()
+  print("tweet")
+
+printit()
+
